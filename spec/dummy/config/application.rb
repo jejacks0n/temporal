7f605c3e5574e04f3040023b54934636eaa954f1
jejacks0n <jejacks0n@gmail.com>
2012-09-21 20:10:29 -0600

    properly remove active_record.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'action_controller/railtie'
-require 'active_record/railtie'
-require 'action_mailer/railtie'
 require 'sprockets/railtie'
 
 require 'evergreen/rails'
@@ -56,7 +54,7 @@ module Dummy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    #config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
