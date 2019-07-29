cdbd2b1af547a846eed76774ea4d1e05daf5ce32
szTheory <szTheory@users.noreply.github.com>
2019-07-28 22:09:08 -0400

    before_filter switch to before_action for rails 5 compat

--- lib/temporal/controller_additions.rb
+++ lib/temporal/controller_additions.rb
@@ -1,7 +1,7 @@
 module Temporal
   module ControllerAdditions
     def self.included(base)
-      base.send(:before_filter, :set_time_zone)
+      base.send(:before_action, :set_time_zone)
     end
 
     def set_time_zone
