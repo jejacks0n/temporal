7bb6e3efc5e5849a3a5a3642ab835692b771a0fe
szTheory <szTheory@users.noreply.github.com>
2019-07-28 22:22:10 -0400

    rails 3 backwards compat

--- lib/temporal/controller_additions.rb
+++ lib/temporal/controller_additions.rb
@@ -1,7 +1,11 @@
 module Temporal
   module ControllerAdditions
     def self.included(base)
-      base.send(:before_action, :set_time_zone)
+      if Rails::VERSION::MAJOR >= 5
+        base.send(:before_action, :set_time_zone)
+      else
+        base.send(:before_filter, :set_time_zone)
+      end
     end
 
     def set_time_zone
